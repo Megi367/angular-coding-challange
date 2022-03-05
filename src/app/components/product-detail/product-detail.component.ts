57e3bddcfc5cb4609ca5bf2b557dcc4d51f29794
Megi Kuka <kukamegi@gmail.com>
2022-03-05 01:22:32 +0100

    Adding New Products feature.

--- src/app/components/product-detail/product-detail.component.ts
+++ src/app/components/product-detail/product-detail.component.ts
@@ -1,7 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { Product } from 'src/app/models/Product';
 import { map } from 'rxjs/operators'
-import { ApiService } from 'src/app/shared/api.service'
+import { ApiService } from 'src/app/shared/api.service';
+import {NgbModal, ModalDismissReasons} from '@ng-bootstrap/ng-bootstrap';
+import {FormGroup, FormBuilder} from '@angular/forms'
+import { ProductModel } from './product-detail.model';
 
 @Component({
   selector: 'app-product-detail',
@@ -11,19 +14,51 @@ import { ApiService } from 'src/app/shared/api.service'
 export class ProductDetailComponent implements OnInit {
 
   products: Product[];
+  closeResult = '';
+  addProductForm!: FormGroup;
+  productModelObject: ProductModel = new ProductModel();
 
-  constructor( private api: ApiService) {
+  constructor( private api: ApiService,
+               private modalService: NgbModal,
+               private formBuilder: FormBuilder) {
     this.products = [];
-
    }
 
   ngOnInit(): void {
     this.getAllProducts();
+
+    this.addProductForm = this.formBuilder.group({
+      name:[''] ,
+      color:[''] ,
+      size: [''],
+      status:[''],
+      conditions:[''] 
+    })
   }
 
+  // CRUDS from the API Service
   getAllProducts(){
     this.api.getProducts()
     .subscribe(res => this.products = res as Product[]);
+  }
+
+  postProductDetails(){
+    this.productModelObject.name = this.addProductForm.value.name;
+    this.productModelObject.color = this.addProductForm.value.color;
+    this.productModelObject.size = this.addProductForm.value.size;
+    this.productModelObject.status = this.addProductForm.value.status;
+    this.productModelObject.conditions = this.addProductForm.value.conditions;
+
+    this.api.postProducts(this.productModelObject)
+    .subscribe(res=>{
+      console.log(res);
+      alert("Product Added Successfully!");
+      this.addProductForm.reset();
+      this.getAllProducts();
+    },
+    err=>{
+      alert("Something Went Wrong!")
+    })
   }
 
   deleteProduct(product: any){
@@ -32,5 +67,29 @@ export class ProductDetailComponent implements OnInit {
       alert("Product Deletet");
       this.getAllProducts();
     })
+  }
+
+
+  // open modal for Adding Products Logic
+  open(content: any) {
+    this.modalService.open(content, {ariaLabelledBy: 'modal-basic-title'}).result.then((result) => {
+      this.closeResult = `Closed with: ${result}`;
+    }, (reason) => {
+      this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+    });
+  }
+
+  private getDismissReason(reason: any): string {
+    if (reason === ModalDismissReasons.ESC) {
+      return 'by pressing ESC';
+    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+      return 'by clicking on a backdrop';
+    } else {
+      return `with: ${reason}`;
+    }
+  }
+
+  closeForm(){
+    this.addProductForm.reset();
   }
 }
---
+++ src/app/components/product-detail/product-detail.model.ts
@@ -0,0 +1,8 @@
+export class ProductModel{
+    id : number = 0;
+    name : string = '';
+    color : string = '';
+    size : string = '';
+    status : string = '';
+    conditions : string = '';
+}
